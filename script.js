2e7933deddc7ef21771b580cfb983b5f8e6885ce
christenally <13616443+Christenally@users.noreply.github.com>
2025-12-31 23:31:09 -0600

    Happy New Years!

--- script.js
+++ script.js
@@ -4,20 +4,24 @@ const userInput = document.getElementById("userInput");
 
 console.log([...userInput.textContent]);
 
+const packedUserInput = [...userInput.textContent];
+
 const analyzeText = {
-  vowelsList: [a, e, i, o, u],
-  wordList: [" ", "."],
-  othersList: ["!", "?", "."],
+  vowelsList: ["a", "e", "i", "o", "u"],
   characterCount: 0,
   wordCount: 0,
   sentenceCount: 0,
-  characterCounter() {},
-  wordCounter() {},
+  characterCounter(arr) {
+    arr.forEach((element) => {
+      element === " " ? null : this.characterCount++;
+    });
+  },
+  wordCounter(str) {
+    this.wordCount += str.split(" ").length;
+  },
   sentenceCounter(arr) {
     arr.forEach((element) => {
-      if (element === ".") {
-        this.sentenceCount++;
-      }
+      element === "." ? this.sentenceCount++ : null;
     });
   },
   densityCounter(arr) {
@@ -36,10 +40,7 @@ const analyzeText = {
   },
 };
 
-// const vowels = {
-//   a: 0,
-//   e: 0,
-//   i: 0,
-//   o: 0,
-//   u: 0,
-// };
+//all functions in this array are based on destructuring in parameter
+
+analyzeText.densityCounter(packedUserInput);
+console.log(analyzeText.vowelsCount);
